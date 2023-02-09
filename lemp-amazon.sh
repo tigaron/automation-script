768d1dab91c748d20be9bea508b24e390adbe09b
tigaron <suputra@mail.com>
2023-02-09 06:44:55 +0000

    chore: server migration

--- lemp-amazon.sh
+++ lemp-amazon.sh
@@ -53,4 +53,29 @@ sudo service php-fpm start
 # sudo yum install certbot python-certbot-nginx -y 
 # sudo certbot certonly --nginx -d fls.red
 
+# Configure NGINX for SSL
+sudo bash -c "cat > /etc/nginx/conf.d/default.conf << 'EOF'
+server {
+  listen 443 http2;
+  listen [::]:443 http2;
+  server_name fls.red;
+
+  
+
+  location / {
+    root   /usr/share/nginx/html;
+    index  index.php index.html index.htm;
+  }
+
+  location ~ \.php$ {
+    root /usr/share/nginx/html;
+    fastcgi_pass   unix:/run/php-fpm/www.sock;
+    fastcgi_index  index.php;
+    fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include        fastcgi_params;
+  }
+}
+EOF
+"
+
 # sudo grep 'temporary password' /var/log/mysqld.log
